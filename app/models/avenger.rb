b32b8e2ef9c0c2f5a55b73df30f33a53f80d71eb
Radha Patel <radha13.patel@gmail.com>
2021-05-31 18:07:38 +0100

    added validations and associations in for all 3 models

--- app/models/avenger.rb
+++ app/models/avenger.rb
@@ -1,3 +1,6 @@
 class Avenger < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  vaidates :price, presence: true
+  vaidates :description, presence: true
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :avenger
+  validates :location, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
