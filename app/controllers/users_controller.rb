d11b4ba8e21c2a2174949d97281d67d7e25559b7
Chloe Balanche <chloebalanche@gmail.com>
2021-06-01 13:10:55 +0100

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.new
+    @bookings = current_user.bookings
+    booking.user == current_user # this is a booking I made for an avenger
+    booking.avenger.user == current_user # this is a booking for one of my avengers
   end
 end
